95684dc4b27548914f8d63a9eb0422f70e6f2f6a
Jacob Robertson <g.jacob.robertson@gmail.com>
2013-03-01 11:18:09 -0500

    renamed brew

--- manifests/2-0-3.pp
+++ manifests/2-0-3.pp
@@ -7,10 +7,10 @@ class grails::2-0-3 {
   include homebrew
 
   homebrew::formula { 'grails':
-    before => Package['boxen/brews/grails-2.0.3'],
+    before => Package['boxen/brews/grails-2-0-3'],
   }
 
-  package { 'boxen/brews/grails-2.0.3':
+  package { 'boxen/brews/grails-2-0-3':
     ensure => '2.0.3-boxen1'
   }
 }
